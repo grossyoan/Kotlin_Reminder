f41b3da34a287972dfbbe1767da4681c3a2a2118
gross.yoan@gmail.com <32864409+grossyoan@users.noreply.github.com>
2019-05-08 22:22:13 +0200

    :construction: Transition between activities and Memo page improved

--- app/src/main/java/com/example/kotlin_reminder/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_reminder/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.kotlin_reminder
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_add_memo.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,28 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
         addButton.setOnClickListener { view ->
-                setContentView(R.layout.activity_add_memo)
-        }
-
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            val MainActivity2AddMemo = Intent(this, AddMemo::class.java)
+            startActivity(MainActivity2AddMemo)
         }
     }
+
 }
--- app/src/main/java/com/example/kotlin_reminder/addMemo.kt
+++ app/src/main/java/com/example/kotlin_reminder/addMemo.kt
@@ -1,18 +1,19 @@
 package com.example.kotlin_reminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_add_memo.*
 
-class addMemo : AppCompatActivity() {
+class AddMemo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_memo)
-        MemoValidateButton.setOnClickListener {
-            Toast.makeText(this, "You clicked me.", Toast.LENGTH_SHORT).show()
+
+        MemoValidateButton.setOnClickListener { view ->
+            val AddMemo2MainActivity = Intent(this, MainActivity::class.java)
+            startActivity(AddMemo2MainActivity)
         }
     }
 }
